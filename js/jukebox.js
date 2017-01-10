05ce6bd60cfa59cb95e425e52b1fc147240f72fe
Laurent Milne <lmilne360@gmail.com>
2017-01-10 06:57:12 +0000

    Done.

--- js/jukebox.js
+++ js/jukebox.js
@@ -15,30 +15,39 @@ function randomNumber() {
 }
 
 function menu() {
-  $('#menu').on('click', function(e) {
-    $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
-  });
+  $('#menu').on('click', options);
 }
 
+
 function play() {
-  $('#play').on('click', function(e) {
-    $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
-    return "now playing " + songToPlay + " by " + theArtist;
-  });
+  $('#play').on('click', nowPlaying);
 }
 
 function pause() {
-  $('#pause').on('click', function(e) {
-    $('#songPaused').html(songToPlay + " is paused");
-  });
+  $('#pause').on('click', songPause);
 }
 
 function show() {
-  $('#show').on('click', function(e) {
-    var songs = "";
-    for (var key in jukeboxSongs) {
+  $('#show').on('click', allSongs);
+}
+/** MINE */
+function options(e) {
+	  $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
+}
+
+function nowPlaying(e) {
+	 $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
+	 return "now playing " + songToPlay + " by " + theArtist;
+}
+
+function songPause(e) {
+	$('#songPaused').html(songToPlay + " is paused");
+}
+
+function allSongs(e){
+  var songs = ""
+	  for (var key in jukeboxSongs) {
       songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
     }
     $('#showSongs').html(songs);
-  });
 }
